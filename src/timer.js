c123027b24b376a370ed125e4bb51e34f57934f1
dopaming <over_ray@naver.com>
2021-07-06 15:11:19 +0900

    timer...

--- src/timer.js
+++ src/timer.js
@@ -4,30 +4,23 @@ import { Link } from 'react-router-dom';
 
 const Timer = ({location}) => {
 
-	const [minutes, setMinutes] = useState(location.state.mm);
-	const [seconds, setSeconds] = useState(location.state.ss);
-  
+	const then = Date.now() + (location.state.mm * 60000) + (location.state.ss * 1000);
+	const [seconds, setSeconds] = useState((then - Date.now()) / 1000);
 	useEffect(() => {
-	  const countdown = setInterval(() => {
-		if (parseInt(seconds) > 0) {
-		  setSeconds(parseInt(seconds) - 1);
-		}
-		if (parseInt(seconds) === 0) {
-		  if (parseInt(minutes) === 0) {
+	const countdown = setInterval(() => {
+		setSeconds(Math.round((then - Date.now()) / 1000));
+		console.log(Date.now());
+		if ((seconds / 1000) < 0) {
 			clearInterval(countdown);
-		  } else {
-			setMinutes(parseInt(minutes) - 1);
-			setSeconds(59);
-		  }
 		}
-	  }, 1000);
-	  return () => clearInterval(countdown);
-	}, [minutes, seconds]);
+	}, 1000);
+	return () => clearInterval(countdown);
+	}, [seconds, then]);
 	return (
 		<div class="container">
 			<Clock />
 			<h1 class="timer">
-				00:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+				00:{Math.floor(seconds / 60)}:{seconds % 60}
 			</h1>
 			<h2 class="link">
 				<Link to = "/">BACK</Link>
@@ -35,5 +28,39 @@ const Timer = ({location}) => {
 		</div>
 	);
   }
+
+// const Timer = ({location}) => {
+
+// 	const [minutes, setMinutes] = useState(location.state.mm);
+// 	const [seconds, setSeconds] = useState(location.state.ss);
+  
+// 	useEffect(() => {
+// 	  const countdown = setInterval(() => {
+// 		if (parseInt(seconds) > 0) {
+// 		  setSeconds(parseInt(seconds) - 1);
+// 		}
+// 		if (parseInt(seconds) === 0) {
+// 		  if (parseInt(minutes) === 0) {
+// 			clearInterval(countdown);
+// 		  } else {
+// 			setMinutes(parseInt(minutes) - 1);
+// 			setSeconds(59);
+// 		  }
+// 		}
+// 	  }, 1000);
+// 	  return () => clearInterval(countdown);
+// 	}, [minutes, seconds]);
+// 	return (
+// 		<div class="container">
+// 			<Clock />
+// 			<h1 class="timer">
+// 				00:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
+// 			</h1>
+// 			<h2 class="link">
+// 				<Link to = "/">BACK</Link>
+// 			</h2>
+// 		</div>
+// 	);
+//   }
 
   export default Timer;
