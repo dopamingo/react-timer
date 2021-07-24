5742ed113e94c3dd7f3255ad8e0a6b63e853ee11
dopaming <over_ray@naver.com>
2021-07-24 22:22:57 +0900

    good

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import App from './app';
-import './reset.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Input from './input';
+import Timer from './timer';
 import './styles.css';
+
+// BrowserRouter - history API를 사용해 URL과 UI를 동기화하는 라우터입니다.
+// Route - 컴포넌트의 속성에 설정된 URL과 현재 경로가 일치하면 해당하는 컴포넌트, 함수를 렌더링한다.
+// Link - 'a'태그와 비슷합니다. to속성에 설정된 링크로 이동합니다. 기록이 history스택에 저장됩니다.
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Route path = "/" component = {Input} exact />
+			<Route path = "/timer" component = {Timer} />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
--- src/timer.js
+++ src/timer.js
@@ -1,26 +1,38 @@
 import React, { useState, useEffect } from 'react';
-import Clock from './clock';
 import { Link } from 'react-router-dom';
 
 const Timer = ({location}) => {
+	const [date, setDate] = useState(new Date());
+	const [minutes, setMinutes] = useState(parseInt(location.state.mm));
+	const [seconds, setSeconds] = useState(parseInt(location.state.ss));
 
-	const then = Date.now() + (location.state.mm * 60000) + (location.state.ss * 1000);
-	const [seconds, setSeconds] = useState((then - Date.now()) / 1000);
 	useEffect(() => {
-	const countdown = setInterval(() => {
-		setSeconds(Math.round((then - Date.now()) / 1000));
-		console.log(Date.now());
-		if ((seconds / 1000) < 0) {
-			clearInterval(countdown);
+		const timerID = setInterval(() => {
+			setDate(new Date());
+		}, 1000);
+		return () => clearInterval(timerID);
+	}, []);
+
+	useEffect(() => {
+		if (seconds > 0) {
+			setSeconds(seconds - 1);
+		  }
+		if (seconds === 0) {
+			if (minutes === 0) {
+				clearInterval();
+			} else {
+				setMinutes(minutes - 1);
+				setSeconds(59);
+			}
 		}
-	}, 1000);
-	return () => clearInterval(countdown);
-	}, [seconds, then]);
+		return () => clearInterval();
+	}, [date]);
+
 	return (
 		<div class="container">
-			<Clock />
+			<h1 class="localtime">{date.toLocaleTimeString('it-IT')}</h1>
 			<h1 class="timer">
-				00:{Math.floor(seconds / 60)}:{seconds % 60}
+				00:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
 			</h1>
 			<h2 class="link">
 				<Link to = "/">BACK</Link>
@@ -28,39 +40,5 @@ const Timer = ({location}) => {
 		</div>
 	);
   }
-
-// const Timer = ({location}) => {
-
-// 	const [minutes, setMinutes] = useState(location.state.mm);
-// 	const [seconds, setSeconds] = useState(location.state.ss);
-  
-// 	useEffect(() => {
-// 	  const countdown = setInterval(() => {
-// 		if (parseInt(seconds) > 0) {
-// 		  setSeconds(parseInt(seconds) - 1);
-// 		}
-// 		if (parseInt(seconds) === 0) {
-// 		  if (parseInt(minutes) === 0) {
-// 			clearInterval(countdown);
-// 		  } else {
-// 			setMinutes(parseInt(minutes) - 1);
-// 			setSeconds(59);
-// 		  }
-// 		}
-// 	  }, 1000);
-// 	  return () => clearInterval(countdown);
-// 	}, [minutes, seconds]);
-// 	return (
-// 		<div class="container">
-// 			<Clock />
-// 			<h1 class="timer">
-// 				00:{minutes < 10 ? `0${minutes}` : minutes}:{seconds < 10 ? `0${seconds}` : seconds}
-// 			</h1>
-// 			<h2 class="link">
-// 				<Link to = "/">BACK</Link>
-// 			</h2>
-// 		</div>
-// 	);
-//   }
 
   export default Timer;
